2e793fe97322ea6bf865fdd968ef1a98c4c6024b
GuanyiLi-Craig <BitForce.Studio@gmail.com>
2022-05-25 22:22:03 +0100

    correct the build command for rpi

--- docker/Spark-Worker/build.sh
+++ docker/Spark-Worker/build.sh
@@ -1,2 +1,2 @@
 docker build -t guanyili/spark-worker:0.0.2 -t guanyili/spark-worker:latest .
-docker build -t guanyili/spark-worker-pi:0.0.2 -t guanyili/spark-worker-pi:latest .
+docker build -t guanyili/spark-worker-pi:0.0.2 -t guanyili/spark-worker-pi:latest -f Dockerfile_pi .
