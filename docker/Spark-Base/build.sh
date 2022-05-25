c21e279ff4aa64560eb8fbcae8ad6771253d9147
GuanyiLi-Craig <BitForce.Studio@gmail.com>
2022-05-25 22:12:47 +0100

    add python support and add build script

---
+++ docker/Spark-Base/build.sh
@@ -0,0 +1,2 @@
+docker build -t guanyili/spark-base:0.0.2 -t guanyili/spark-base:latest .
+docker build -t guanyili/spark-base-pi:0.0.2 -t guanyili/spark-base-pi:latest .
---
+++ docker/Spark-Master/build.sh
@@ -0,0 +1,2 @@
+docker build -t guanyili/spark-master:0.0.2 -t guanyili/spark-master:latest .
+docker build -t guanyili/spark-master-pi:0.0.2 -t guanyili/spark-master-pi:latest .
